3c514df6d8c5fae9fd3db5bc275788edd7f91ee2
FiggyHunter <leonardo.roi3@gmail.com>
2023-08-16 15:05:24 +0200

    added components

---
+++ Project1/S3/project-3/src/components/Main.tsx
@@ -0,0 +1,15 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousand of enterprise apps, including mobile apps</li>
+      </ul>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"></img>
+    </main>
+  );
+}
---
+++ Project1/S3/project-3/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <div className="logo">
+        <img
+          width={"50px"}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+          alt="logo react"
+        />
+        <h1>ReactFacts</h1>
+      </div>
+      <p>React Course - Project 1</p>
+    </nav>
+  );
+}
