d63f468572f2c82809f26ec09487b09a6c1166c0
FiggyHunter <leonardo.roi3@gmail.com>
2023-08-18 23:57:31 +0200

    added data for the airbnb exp clone

---
+++ Project2/S1/src/data.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 1,
+    title: "Life Lessons with Katie Zaferes",
+    description:
+      'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
+    price: 136,
+    coverImg:
+      "https://plus.unsplash.com/premium_photo-1667533288519-49d00a1620b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+    location: "Online",
+    openSpots: 0,
+  },
+  {
+    id: 2,
+    title: "Learn Wedding Photography",
+    description:
+      "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+    price: 125,
+    coverImg:
+      "https://images.unsplash.com/photo-1583939003579-730e3918a45a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+    location: "Online",
+    openSpots: 27,
+  },
+  {
+    id: 3,
+    title: "Group Mountain Biking",
+    description:
+      "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+    price: 50,
+    coverImg:
+      "https://images.unsplash.com/photo-1594032362338-04b9952eb440?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80",
+    stats: {
+      rating: 4.8,
+      reviewCount: 2,
+    },
+    location: "Norway",
+    openSpots: 3,
+  },
+];
